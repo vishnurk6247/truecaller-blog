a709ed90f709d1696a6df93b6ada3947f7dc4398
vishnu r kumar <rkumar.vishnu28@gmail.com>
2021-11-27 00:20:41 +0530

    made folder structure for the app

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Hello world!" />
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body {
+  margin: 0;
+  font-family: "Montserrat", sans-serif;
+  font-size: 16px;
+  color: rgba(0, 0, 0, 0.87);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
