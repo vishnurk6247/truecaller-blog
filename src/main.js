4d4faad5db69caac40ac3432e7a2ba1bf6dc8d0c
vishnu r kumar <rkumar.vishnu28@gmail.com>
2021-11-28 21:48:01 +0530

    added paginator component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faChevronLeft,
+  faChevronRight,
+  faAngleDoubleRight,
+  faAngleDoubleLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+library.add(
+  faChevronLeft,
+  faChevronRight,
+  faAngleDoubleRight,
+  faAngleDoubleLeft
+);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
@@ -9,4 +25,3 @@ new Vue({
   render: (h) => h(App),
   router: router,
 }).$mount("#app");
-
